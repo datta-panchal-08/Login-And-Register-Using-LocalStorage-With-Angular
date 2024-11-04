c879fd8427088b0ca4a97dd261ec5d44ec9c19fc
datta panchal <dattapanchal464@gmail.com>
2024-11-04 18:01:16 +0530

    completed

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  loginFrm!:FormGroup;
+  isLogin:boolean = true;
+  constructor(private fb:FormBuilder, private router:Router) { }
+ 
   ngOnInit(): void {
+    this.loginFrm = this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',Validators.required],
+      userName:['',Validators.required]
+    })
+  }
+
+  onSubmit(){
+   const isLocalData  = localStorage.getItem('angular14');
+   const formData = this.loginFrm.value;
+  if(isLocalData != null){
+      const localArray = JSON.parse(isLocalData); 
+      localArray.push(formData);
+      localStorage.setItem('angular14',JSON.stringify(localArray));
+  }else{
+    const localArray = [];
+    localArray.push(formData);
+    localStorage.setItem('angular14',JSON.stringify(localArray));
+  }
+  }
+
+  onLogin(){
+    const isLocalData  = localStorage.getItem('angular14'); 
+    if(isLocalData != null){
+      const users = JSON.parse(isLocalData);
+
+      const isUserFound = users.find((m:any) => m.email === this.loginFrm.value.email && m.password === this.loginFrm.value.password);
+
+      if(isUserFound != undefined){
+        this.router.navigate(['/dashboard']);
+      }else{
+        alert("Username or Password is wrong!")
+      }
+    }else{
+      alert("No User Found");
+    }
   }
 
 }
